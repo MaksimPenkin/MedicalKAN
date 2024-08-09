91b6ca77c8ace4192558b52c4d68ac0f4922ad70
MaksimPenkin <penkin97@gmail.com>
2024-08-09 14:42:46 +0300

    txt sampler

--- datasets/samplers/textline_sampler.py
+++ datasets/samplers/textline_sampler.py
@@ -5,14 +5,14 @@
 from data.samplers.base_sampler import IterableSampler
 
 
-class TextLineSampler(IterableSampler):
+class TXTSampler(IterableSampler):
 
     @property
     def file(self):
         return self._file
 
     def __init__(self, filename):
-        super(TextLineSampler, self).__init__()
+        super(TXTSampler, self).__init__()
 
         with open(filename, "rt") as f:
             self._file = f.read().splitlines()
@@ -21,6 +21,6 @@ class TextLineSampler(IterableSampler):
         return len(self.file)
 
     def __getitem__(self, item):
-        super(TextLineSampler, self).__getitem__(item)
+        super(TXTSampler, self).__getitem__(item)
 
         return self.file[item]
