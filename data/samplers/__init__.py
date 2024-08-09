ab809ea3d7d591ee62f521fca4fd597750c56bbf
MaksimPenkin <penkin97@gmail.com>
2024-08-09 15:54:15 +0300

    def get(...) for samplers

--- data/samplers/__init__.py
+++ data/samplers/__init__.py
@@ -3,5 +3,20 @@
 # """
 
 from data.samplers.base_sampler import ISampler
-from data.samplers.textline_sampler import TextLineSampler
-from data.samplers.textpath_sampler import TextPathSampler
+from data.samplers.csv_sampler import CSVSampler
+from data.samplers.txt_sampler import TXTSampler
+
+from utils.serialization_utils import create_object
+
+
+def get(identifier, **kwargs):
+    obj = create_object(identifier,
+                        module_objects={
+                            "csv": CSVSampler,
+                            "txt": TXTSampler
+                        },
+                        **kwargs)
+
+    if isinstance(obj, ISampler):
+        return obj
+    raise ValueError(f"Could not interpret sampler instance: {obj}.")
