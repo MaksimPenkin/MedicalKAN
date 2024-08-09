8040329c6d2ce72c67cee815d6bf0a3c3aaf317c
MaksimPenkin <penkin97@gmail.com>
2024-08-09 16:05:11 +0300

    kan v0 added

---
+++ nn/models/kan_v0.py
@@ -0,0 +1,27 @@
+# """
+# @author   Maksim Penkin
+# """
+
+import torch
+from torch import nn
+from nn.layers.kan_convolutional.KANConv import KAN_Convolutional_Layer
+
+
+class KAN_v0(nn.Module):
+    def __init__(self, device: str = 'cpu'):
+        super().__init__()
+
+        self.conv_kan = KAN_Convolutional_Layer(
+            n_convs=16,
+            kernel_size=(3, 3),
+            padding=(1, 1),
+            device=device
+        )
+
+        self.restore = torch.nn.Conv2d(16, 1, 3, padding=1)
+
+    def forward(self, x):
+        x = self.conv_kan(x)
+        x = self.restore(x)
+
+        return x
