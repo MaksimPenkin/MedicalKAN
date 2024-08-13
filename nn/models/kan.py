11a76ffdc40f902fc2efb6bdce8435bf8e21485f
MaksimPenkin <penkin97@gmail.com>
2024-08-13 14:36:08 +0300

    kan zoo added, depending on n_convs + yaml configs.

--- nn/models/kan.py
+++ nn/models/kan.py
@@ -8,17 +8,17 @@ from nn.layers.kan_convolutional.KANConv import KAN_Convolutional_Layer
 
 
 class KAN_v0(nn.Module):
-    def __init__(self, device="cpu"):
+    def __init__(self, n_convs=16, device="cpu"):
         super().__init__()
 
         self.conv_kan = KAN_Convolutional_Layer(
-            n_convs=16,
+            n_convs=n_convs,
             kernel_size=(3, 3),
             padding=(1, 1),
             device=device
         )
 
-        self.restore = torch.nn.Conv2d(16, 1, 3, padding=1)
+        self.restore = torch.nn.Conv2d(n_convs, 1, 3, padding=1)
 
     def forward(self, x):
         x = self.conv_kan(x)
