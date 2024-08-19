a3fdbc372f72dcf460e6e8601742069633fcd7ea
MaksimPenkin <penkin97@gmail.com>
2024-08-19 16:48:31 +0300

    minor

--- nn/models/ukan.py
+++ nn/models/ukan.py
@@ -18,7 +18,7 @@ class _PatchEmbedding(nn.Module):
         super(_PatchEmbedding, self).__init__()
 
         patch_size = to_2tuple(patch_size)
-        self.conv = nn.Conv2d(in_ch, embed_ch, patch_size,
+        self.emb = nn.Conv2d(in_ch, embed_ch, patch_size,
                               stride=stride,
                               padding=(patch_size[0] // 2, patch_size[1] // 2))
 
@@ -40,28 +40,33 @@ class _PatchEmbedding(nn.Module):
                 m.bias.data.zero_()
 
     def forward(self, x):
-        x = self.conv(x)
+        x = self.emb(x)
         _, _, H, W = x.shape
         x = x.flatten(start_dim=2).transpose(1, 2)
 
         return x, H, W
 
 
-class _KANEmbedding(nn.Module):
+class KANBottleneckBlock(nn.Module):
 
-    def __init__(self, dim):
-        super(_KANEmbedding, self).__init__()
+    def __init__(self, dim, version="spline"):
+        super(KANBottleneckBlock, self).__init__()
 
-        kan_kwargs = dict(grid_size=5,
-                          spline_order=3,
-                          scale_noise=0.1,
-                          scale_base=1.0,
-                          scale_spline=1.0,
-                          base_activation=torch.nn.SiLU,
-                          grid_eps=0.02,
-                          grid_range=[-1, 1])
+        if version == "spline":
+            self.kan = KANLinear(dim, dim,
+                                 grid_size=5,
+                                 spline_order=3,
+                                 scale_noise=0.1,
+                                 scale_base=1.0,
+                                 scale_spline=1.0,
+                                 base_activation=torch.nn.SiLU,
+                                 grid_eps=0.02,
+                                 grid_range=[-1, 1])
+        else:
+            raise NotImplementedError(f"Unrecognized `version` found: {version}.")
 
-        self.fc = KANLinear(dim, dim, **kan_kwargs)
+        self.norm = nn.LayerNorm(dim)
+
         self.dwconv = nn.Conv2d(dim, dim, 3, stride=1, padding=1, groups=dim)
         self.bn = nn.BatchNorm2d(dim)
         self.relu = nn.ReLU()
@@ -85,24 +90,25 @@ class _KANEmbedding(nn.Module):
 
     def forward(self, x, H, W):
         B, N, C = x.shape
+        identity = x
 
         x = x.reshape(B * N, C)
-        x = self.fc(x)
+        x = self.kan(self.norm(x))
         x = x.reshape(B, N, C).contiguous()
 
         x = x.transpose(1, 2).view(B, C, H, W)
         x = self.relu(self.bn(self.dwconv(x)))
         x = x.flatten(start_dim=2).transpose(1, 2)
 
-        return x
+        return identity + x
 
 
-class EncoderBlock(nn.Module):
+class ConvEncoderBlock(nn.Module):
 
     def __init__(self, in_ch, out_ch):
-        super(EncoderBlock, self).__init__()
+        super(ConvEncoderBlock, self).__init__()
 
-        self.layer = nn.Sequential(
+        self.conv = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
@@ -112,44 +118,15 @@ class EncoderBlock(nn.Module):
         )
 
     def forward(self, x):
-        return self.layer(x)
+        return self.conv(x)
 
 
-class BottleneckBlock(nn.Module):
-
-    def __init__(self, dim):
-        super(BottleneckBlock, self).__init__()
-
-        self.layer = _KANEmbedding(dim)
-        self.norm = nn.LayerNorm(dim)
-
-        self.apply(self._init_weights)
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=.02)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-        elif isinstance(m, nn.Conv2d):
-            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-            fan_out //= m.groups
-            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
-            if m.bias is not None:
-                m.bias.data.zero_()
-
-    def forward(self, x, H, W):
-        return x + self.layer(self.norm(x), H, W)
-
-
-class DecoderBlock(nn.Module):
+class ConvDecoderBlock(nn.Module):
 
     def __init__(self, in_ch, out_ch):
-        super(DecoderBlock, self).__init__()
+        super(ConvDecoderBlock, self).__init__()
 
-        self.layer = nn.Sequential(
+        self.conv = nn.Sequential(
             nn.Conv2d(in_ch, in_ch, 3, padding=1),
             nn.BatchNorm2d(in_ch),
             nn.ReLU(inplace=True),
@@ -159,4 +136,4 @@ class DecoderBlock(nn.Module):
         )
 
     def forward(self, x):
-        return self.layer(x)
+        return self.conv(x)
