a34814adc2f0567050e8602ef68605a7f3ad30af
Maksim Penkin <penkin97@gmail.com>
2024-11-22 17:50:34 +0300

    minor: models

--- nn/models/__init__.py
+++ nn/models/__init__.py
@@ -6,6 +6,7 @@ import torch
 from nn.models.ckan import ConvKAN
 from nn.models.ukan import StackedResidualKAN
 from nn.models.resnet import resnet18, resnet50, resnet101
+from utils.torch_utils import torch_load
 
 from utils.serialization_utils import create_object
 
@@ -22,6 +23,6 @@ def get(identifier, checkpoint=None, **kwargs):
 
     if isinstance(obj, torch.nn.Module):
         if checkpoint:
-            obj.load_state_dict(torch.load(checkpoint, map_location="cpu", weights_only=True))
+            obj = torch_load(obj, checkpoint, strict=False)
         return obj
     raise ValueError(f"Could not interpret model instance: {obj}.")
