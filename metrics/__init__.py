7e0ffcddac8ecf5d86cd4a45d976bcb9c5002c94
Maksim Penkin <penkin97@gmail.com>
2024-11-22 16:39:57 +0300

    metrics synch

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -2,7 +2,8 @@
 # @author   Maksim Penkin
 # """
 
-from metrics.base_metric import IMetric, MeanMetric
+from metrics.base_metric import IMetric
+from metrics.mean_metric import MeanMetric
 
 from utils.serialization_utils import create_object
 
@@ -10,9 +11,34 @@ from utils.serialization_utils import create_object
 def get(identifier, **kwargs):
     obj = create_object(identifier,
                         module_objects={
-                            "mean": MeanMetric},
+                            "MeanMetric": MeanMetric,
+                            "CompositeMetric": CompositeMetric},
                         **kwargs)
 
     if isinstance(obj, IMetric):
         return obj
     raise ValueError(f"Could not interpret metric instance: {obj}.")
+
+
+class CompositeMetric(IMetric):
+
+    def __init__(self, metrics=None, name=None):
+        super(CompositeMetric, self).__init__(name=name)
+
+        if metrics:
+            self.metrics = {metric.name: metric for metric in [get(m) for m in metrics]}
+        else:
+            self.metrics = {}
+
+    def reset_state(self):
+        for m in self.metrics.values():
+            m.reset_state()
+
+    def update_state(self, value, n=1):
+        for k, v in value.items():
+            if k not in self.metrics:
+                self.metrics[k] = MeanMetric(name=k)
+            self.metrics[k].update_state(v, n=n)
+
+    def result(self):
+        return {m.name: m.result() for m in self.metrics.values()}
--- metrics/base_metric.py
+++ metrics/base_metric.py
@@ -12,13 +12,9 @@ class IMetric:
         return self._name
 
     def __init__(self, name=None):
-        if name is None:
-            self._name = "metric"
-        elif isinstance(name, str):
-            self._name = name
-        else:
-            raise ValueError("metrics/base_metric.py: class IMetric: def __init__(...): "
-                             f"error: expected `name` to be string or None, found: {name} of type {type(name)}.")
+        name = name or "metric"
+        assert isinstance(name, str)
+        self._name = name
 
     @abc.abstractmethod
     def reset_state(self):
@@ -31,61 +27,3 @@ class IMetric:
     @abc.abstractmethod
     def result(self):
         raise NotImplementedError("Must be implemented in subclasses.")
-
-
-class MeanMetric(IMetric):
-
-    @property
-    def total(self):
-        return self._total
-
-    @property
-    def count(self):
-        return self._count
-
-    def __init__(self, *args, **kwargs):
-        super(MeanMetric, self).__init__(*args, **kwargs)
-
-        self._total = 0.0
-        self._count = 0
-
-        self.reset_state()
-
-    def reset_state(self):
-        self._total = 0.0
-        self._count = 0
-
-    def update_state(self, value, n=1):
-        self._total += value * n
-        self._count += n
-
-    def result(self):
-        if self._count == 0:
-            return 0.0
-        else:
-            return self._total / self._count
-
-
-class CompositeMetric(IMetric):
-
-    def __init__(self, metrics=None, name=None):
-        super(CompositeMetric, self).__init__(name=name)
-
-        if metrics:
-            assert all(isinstance(m, IMetric) for m in metrics), f"Error: expected all elements of `metrics` IMetric, found: {metrics}."
-            self.metrics = {m.name: m for m in metrics}
-        else:
-            self.metrics = {}
-
-    def reset_state(self):
-        for m in self.metrics.values():
-            m.reset_state()
-
-    def update_state(self, value, n=1):
-        for k, v in value.items():
-            if k not in self.metrics:
-                self.metrics[k] = MeanMetric(name=k)
-            self.metrics[k].update_state(v, n=n)
-
-    def result(self):
-        return {m.name: m.result() for m in self.metrics.values()}
