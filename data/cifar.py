35efd75148bd30173212041941f173c1ed158274
Maksim Penkin <penkin97@gmail.com>
2024-11-26 13:13:17 +0300

    mnist and cifar datasets added.

---
+++ data/cifar.py
@@ -0,0 +1,25 @@
+# """
+# @author   Maksim Penkin
+# """
+
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+from utils.constants import CIFAR10_MEAN, CIFAR10_STD, CIFAR100_MEAN, CIFAR100_STD
+
+
+def cifar10(root="/media/datasets/CIFAR10", split="val", **kwargs):
+    db = torchvision.datasets.CIFAR10(root, train=(split == "train"), download=True,
+                                      transform=transforms.Compose([transforms.ToTensor(),
+                                                                    transforms.Normalize(mean=CIFAR10_MEAN,
+                                                                                         std=CIFAR10_STD)]))
+    return DataLoader(db, **kwargs)
+
+
+def cifar100(root="/media/datasets/CIFAR100", split="val", **kwargs):
+    db = torchvision.datasets.CIFAR100(root, train=(split == "train"), download=True,
+                                       transform=transforms.Compose([transforms.ToTensor(),
+                                                                     transforms.Normalize(mean=CIFAR100_MEAN,
+                                                                                          std=CIFAR100_STD)]))
+    return DataLoader(db, **kwargs)
