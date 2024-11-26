5703dd83e60d4d008caa4ca157d672aeb865f5f0
Maksim Penkin <penkin97@gmail.com>
2024-11-26 18:22:42 +0300

    minor

--- data/samplers/random_sampler.py
+++ data/samplers/random_sampler.py
@@ -2,7 +2,6 @@
 # @author   Maksim Penkin
 # """
 
-from itertools import zip_longest
 from utils.torch_utils import torch_random
 
 from data.samplers.base_sampler import IterableSampler
@@ -11,24 +10,13 @@ from data.samplers.base_sampler import IterableSampler
 class UniformSampler(IterableSampler):
 
     @property
-    def shapes(self):
-        return self._shapes
-
-    @property
     def parameters(self):
         return self._parameters
 
-    @property
-    def dtypes(self):
-        return self._dtypes
-
-    def __init__(self, shapes, parameters=None, dtypes=None, n=1):
+    def __init__(self, parameters=None, n=1):
         super(UniformSampler, self).__init__()
 
-        self._shapes = shapes
-        self._parameters = parameters
-        self._dtypes = dtypes
-
+        self._parameters = parameters if parameters is not None else []
         assert n >= 0, f"The number of items in a collection can't be negative, found: {n}."
         self._n = n
 
@@ -36,7 +24,4 @@ class UniformSampler(IterableSampler):
         return self._n
 
     def __getitem__(self, idx):
-        return tuple(
-            torch_random(shape, low=low, high=high, dtype=dtype)
-            for shape, (low, high), dtype in zip_longest(self.shapes, self.parameters, self.dtypes)
-        )
+        return tuple(torch_random(**kwargs) for kwargs in self.parameters)
