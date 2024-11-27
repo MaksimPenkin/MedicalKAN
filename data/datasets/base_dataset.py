5b3d00c404020bf216201fe26e03c28b6f501175
Maksim Penkin <penkin97@gmail.com>
2024-11-27 11:47:43 +0300

    code review of the datasets -- ok

--- data/datasets/base_dataset.py
+++ data/datasets/base_dataset.py
@@ -10,27 +10,39 @@ from torch.utils.data import Dataset
 
 class CustomDataset(Dataset):
 
+    @property
+    def root(self):
+        return self._root
+
+    @property
+    def transforms(self):
+        return self._transforms
+
     def __init__(self, root, transforms=None):
-        self.root = root
+        self._root = root
 
         if transforms is not None:
-            self.transforms = augmentations.CompositeTransform([augmentations.get(t) for t in transforms])
+            self._transforms = augmentations.CompositeTransform([augmentations.get(t) for t in transforms])
         else:
-            self.transforms = None
+            self._transforms = None
 
     def __len__(self):
         raise NotImplementedError("Must be implemented in subclasses.")
 
-    def __getitem__(self, idx):
+    def __getitem__(self, index):
         raise NotImplementedError("Must be implemented in subclasses.")
 
 
 class SamplerDataset(CustomDataset):
 
+    @property
+    def sampler(self):
+        return self._sampler
+
     def __init__(self, sampler, *args, **kwargs):
         super(SamplerDataset, self).__init__(*args, **kwargs)
 
-        self.sampler = samplers.get(sampler)
+        self._sampler = samplers.get(sampler)
 
     def __len__(self):
         return len(self.sampler)
--- data/datasets/image_dataset.py
+++ data/datasets/image_dataset.py
@@ -11,25 +11,25 @@ from data.datasets.base_dataset import SamplerDataset
 
 class ImageDataset(SamplerDataset):
 
-    def __init__(self, *args, load_params=None, return_filename=False, **kwargs):
+    def __init__(self, *args, load_func=None, load_params=None, return_filenames=False, **kwargs):
         super(ImageDataset, self).__init__(*args, **kwargs)
 
-        self.load_func = read_img  # Hard-coded loader.
-        self.load_params = load_params if load_params is not None else []
-        self.return_filename = bool(return_filename)
+        self._load_func = load_func or read_img
+        self._load_params = load_params or []
+        self.return_filenames = bool(return_filenames)
 
-    def __getitem__(self, idx):
-        filenames = self.sampler[idx]
+    def __getitem__(self, index):
+        filenames = self.sampler[index]
 
         sample = tuple(
-            self.load_func(os.path.join(self.root, filename), **kwargs)
-            for filename, kwargs in zip_longest(filenames, self.load_params, fillvalue={})
+            self._load_func(os.path.join(self.root, filename), **kwargs)
+            for filename, kwargs in zip_longest(filenames, self._load_params, fillvalue={})
         )
 
         if self.transforms:
             sample = self.transforms(*sample)
 
-        if self.return_filename:
+        if self.return_filenames:
             return *sample, os.path.split(filenames[0])[-1]
         else:
             return sample
