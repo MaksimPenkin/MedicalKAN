bddf59a02c14ba792b79b19bccc0af17bd62fb18
Maksim Penkin <penkin97@gmail.com>
2024-11-27 09:39:06 +0300

    minor fixes: code style and usage.

--- data/samplers/random_sampler.py
+++ data/samplers/random_sampler.py
@@ -10,18 +10,20 @@ from data.samplers.base_sampler import IterableSampler
 class UniformSampler(IterableSampler):
 
     @property
-    def parameters(self):
-        return self._parameters
+    def random_vector(self):
+        return self._random_vector
 
-    def __init__(self, parameters=None, n=1):
+    def __init__(self, random_vector, n=1):
         super(UniformSampler, self).__init__()
 
-        self._parameters = parameters if parameters is not None else []
-        assert n >= 0, f"The number of items in a collection can't be negative, found: {n}."
+        assert isinstance(random_vector, (list, tuple))
+        self._random_vector = random_vector
+
+        assert isinstance(n, int) and n >= 0, f"The number of items in a collection can't be negative or non-integer, found: {n}."
         self._n = n
 
     def __len__(self):
         return self._n
 
     def __getitem__(self, idx):
-        return tuple(torch_random(**kwargs) for kwargs in self.parameters)
+        return tuple(torch_random(**kwargs) for kwargs in self.random_vector)
