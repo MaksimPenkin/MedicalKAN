49862446fb7acea1ea1cbe145b3ca0f7ea4bdf0c
Maksim Penkin <penkin97@gmail.com>
2024-11-28 09:23:09 +0300

    logic of calling transforms updated and generalized to an usable types.

--- data/transforms/base_transform.py
+++ data/transforms/base_transform.py
@@ -11,5 +11,13 @@ class ITransform:
         pass
 
     @abc.abstractmethod
-    def __call__(self, *args):
+    def _fn(self, x):
         raise NotImplementedError("Must be implemented in subclasses.")
+
+    def __call__(self, sample):
+        if isinstance(sample, dict):
+            return {k: self._fn(v) for k, v in sample.items()}
+        elif isinstance(sample, (list, tuple)):
+            return [self._fn(x) for x in sample]
+        else:
+            return self._fn(sample)
--- data/transforms/totensor_transform.py
+++ data/transforms/totensor_transform.py
@@ -12,5 +12,5 @@ class ToTensor(ITransform):
     def __init__(self):
         super(ToTensor, self).__init__()
 
-    def __call__(self, *args):
-        return tuple(F.to_tensor(x) for x in args)
+    def _fn(self, x):
+        return F.to_tensor(x)
