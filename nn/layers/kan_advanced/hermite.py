fb7dec39460e814ad37405856c3c3c263b4bd245
Maksim Penkin <penkin97@gmail.com>
2024-12-24 12:13:15 +0300

    hermite functions scaling added to fit [-1,1] finite grid (hermite functions are localized.)

--- nn/layers/kan_advanced/hermite.py
+++ nn/layers/kan_advanced/hermite.py
@@ -35,18 +35,22 @@ class HermiteKANLayer(nn.Module):
 
 
 class HermiteFuncKANLayer(nn.Module):
-    def __init__(self, input_dim, output_dim, degree):
+    def __init__(self, input_dim, output_dim, degree, eps=1):
         super(HermiteFuncKANLayer, self).__init__()
         self.input_dim = input_dim
         self.out_dim = output_dim
         self.degree = degree
+        self.eps = eps
 
         self.hermite_coeffs = nn.Parameter(torch.empty(input_dim, output_dim, degree + 1))
         nn.init.normal_(self.hermite_coeffs, mean=0.0, std=1 / (input_dim * (degree + 1)))
 
     def forward(self, x):
         x = torch.reshape(x, (-1, self.input_dim))
+        # We need to normalize x to [-1, 1] using tanh
+        x = torch.tanh(x) * (math.sqrt(2 * self.degree + 1) + self.eps)
         hermite = torch.ones(x.shape[0], self.input_dim, self.degree + 1, device=x.device)
+        hermite[:, :, 0] = math.pi ** (-1 / 4) * torch.exp(-(x ** 2) / 2)
         if self.degree > 0:
             hermite[:, :, 1] = math.sqrt(2) * math.pi ** (-1 / 4) * x * torch.exp(-(x ** 2) / 2)
         for i in range(2, self.degree + 1):
