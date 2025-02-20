721aeeaa4aeb804d0b800486e90589f6cd82b525
Maksim Penkin <penkin97@gmail.com>
2025-02-20 17:07:22 +0300

    hermite kan: either to return eisum activation of return just basis funcs

--- nn/layers/kan_advanced/hermite.py
+++ nn/layers/kan_advanced/hermite.py
@@ -9,15 +9,16 @@ import torch.nn as nn
 
 
 class HermiteKANLinear(nn.Module):
-    def __init__(self, input_dim, output_dim, degree):
+    def __init__(self, input_dim, output_dim, degree, einsum=True):
         super(HermiteKANLinear, self).__init__()
         self.inputdim = input_dim
         self.outdim = output_dim
         self.degree = degree
 
-        # Initialize Hermite polynomial coefficients
-        self.hermite_coeffs = nn.Parameter(torch.empty(input_dim, output_dim, degree + 1))
-        nn.init.normal_(self.hermite_coeffs, mean=0.0, std=1 / (input_dim * (degree + 1)))
+        self.einsum = bool(einsum)
+        if self.einsum:
+            self.hermite_coeffs = nn.Parameter(torch.empty(input_dim, output_dim, degree + 1))
+            nn.init.normal_(self.hermite_coeffs, mean=0.0, std=1 / (input_dim * (degree + 1)))
 
     def forward(self, x):
         x = torch.reshape(x, (-1, self.inputdim))
@@ -28,21 +29,26 @@ class HermiteKANLinear(nn.Module):
             hermite[:, :, 1] = 2 * x
         for i in range(2, self.degree + 1):
             hermite[:, :, i] = 2 * x * hermite[:, :, i - 1].clone() - 2 * (i - 1) * hermite[:, :, i - 2].clone()
-        y = torch.einsum('bid,iod->bo', hermite, self.hermite_coeffs)
-        y = y.view(-1, self.outdim)
-        return y
+
+        if self.einsum:
+            y = torch.einsum('bid,iod->bo', hermite, self.hermite_coeffs)
+            return y.view(-1, self.outdim)
+        else:
+            return hermite
 
 
 class HermiteFuncKANLinear(nn.Module):
-    def __init__(self, input_dim, output_dim, degree, eps=1):
+    def __init__(self, input_dim, output_dim, degree, eps=1, einsum=True):
         super(HermiteFuncKANLinear, self).__init__()
         self.inputdim = input_dim
         self.outdim = output_dim
         self.degree = degree
         self.eps = eps
 
-        self.hermite_coeffs = nn.Parameter(torch.empty(input_dim, output_dim, degree + 1))
-        nn.init.normal_(self.hermite_coeffs, mean=0.0, std=1 / (input_dim * (degree + 1)))
+        self.einsum = bool(einsum)
+        if self.einsum:
+            self.hermite_coeffs = nn.Parameter(torch.empty(input_dim, output_dim, degree + 1))
+            nn.init.normal_(self.hermite_coeffs, mean=0.0, std=1 / (input_dim * (degree + 1)))
 
     def forward(self, x):
         x = torch.reshape(x, (-1, self.inputdim))
@@ -54,6 +60,9 @@ class HermiteFuncKANLinear(nn.Module):
             hermite[:, :, 1] = math.sqrt(2) * math.pi ** (-1 / 4) * x * torch.exp(-(x ** 2) / 2)
         for i in range(2, self.degree + 1):
             hermite[:, :, i] = math.sqrt(2 / i) * x * hermite[:, :, i - 1].clone() - math.sqrt((i - 1) / i) * hermite[:, :, i - 2].clone()
-        y = torch.einsum('bid,iod->bo', hermite, self.hermite_coeffs)
-        y = y.view(-1, self.outdim)
-        return y
+
+        if self.einsum:
+            y = torch.einsum('bid,iod->bo', hermite, self.hermite_coeffs)
+            return y.view(-1, self.outdim)
+        else:
+            return hermite
