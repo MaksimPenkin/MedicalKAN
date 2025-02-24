d33a7e13ae38852922734c8267c8279252902c25
Maksim Penkin <penkin97@gmail.com>
2025-02-24 12:35:56 +0300

    refactor: filedataset: pathlib instead of os + root is moved here..

--- data/datasets/file_dataset.py
+++ data/datasets/file_dataset.py
@@ -2,7 +2,7 @@
 # @author   Maksim Penkin
 # """
 
-import os
+from pathlib import Path
 from utils.serialization_utils import create_func
 
 from data.datasets.sampler_dataset import SamplerDataset
@@ -10,21 +10,26 @@ from data.datasets.sampler_dataset import SamplerDataset
 
 class FileDataset(SamplerDataset):
 
-    def __init__(self, load_func, load_params, *args, **kwargs):
+    @property
+    def root(self):
+        return self._root
+
+    def __init__(self, root, load_func, load_params, *args, **kwargs):
         super(FileDataset, self).__init__(*args, **kwargs)
 
+        self._root = Path(root)
         self._load_func = create_func(load_func)
         self._load_params = load_params
 
     def _load(self, x):
         if isinstance(x, dict):
-            return {k: self._load_func(os.path.join(self.root, filename), **kwargs)
+            return {k: self._load_func(self.root / filename, **kwargs)
                     for (k, filename), kwargs in zip(x.items(), self._load_params)}
         elif isinstance(x, (list, tuple)):
-            return [self._load_func(os.path.join(self.root, filename), **kwargs)
+            return [self._load_func(self.root / filename, **kwargs)
                     for filename, kwargs in zip(x, self._load_params)]
         else:
-            return self._load_func(os.path.join(self.root, x), **self._load_params)
+            return self._load_func(self.root / x, **self._load_params)
 
     def __getitem__(self, index):
         sample = self._load(self.sampler[index])
