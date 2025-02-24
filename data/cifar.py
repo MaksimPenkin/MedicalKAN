ec6d9616687dbe3a511072ad069faeef7e8f81f2
Maksim Penkin <penkin97@gmail.com>
2025-02-24 11:20:10 +0300

    import clean

--- data/cifar.py
+++ data/cifar.py
@@ -2,24 +2,23 @@
 # @author   Maksim Penkin
 # """
 
-import torchvision
-import torchvision.transforms as transforms
+from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 from utils.constants import CIFAR10_MEAN, CIFAR10_STD, CIFAR100_MEAN, CIFAR100_STD
 
 
 def cifar10(root="/media/datasets/CIFAR10", split="val", **kwargs):
-    db = torchvision.datasets.CIFAR10(root, train=(split == "train"), download=True,
-                                      transform=transforms.Compose([transforms.ToTensor(),
-                                                                    transforms.Normalize(mean=CIFAR10_MEAN,
-                                                                                         std=CIFAR10_STD)]))
+    db = datasets.CIFAR10(root, train=(split == "train"), download=True,
+                          transform=transforms.Compose([transforms.ToTensor(),
+                                                        transforms.Normalize(mean=CIFAR10_MEAN,
+                                                                             std=CIFAR10_STD)]))
     return DataLoader(db, **kwargs)
 
 
 def cifar100(root="/media/datasets/CIFAR100", split="val", **kwargs):
-    db = torchvision.datasets.CIFAR100(root, train=(split == "train"), download=True,
-                                       transform=transforms.Compose([transforms.ToTensor(),
-                                                                     transforms.Normalize(mean=CIFAR100_MEAN,
-                                                                                          std=CIFAR100_STD)]))
+    db = datasets.CIFAR100(root, train=(split == "train"), download=True,
+                           transform=transforms.Compose([transforms.ToTensor(),
+                                                         transforms.Normalize(mean=CIFAR100_MEAN,
+                                                                              std=CIFAR100_STD)]))
     return DataLoader(db, **kwargs)
--- data/mnist.py
+++ data/mnist.py
@@ -2,16 +2,15 @@
 # @author   Maksim Penkin
 # """
 
-import torchvision
-import torchvision.transforms as transforms
+from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 from utils.constants import MNIST_MEAN, MNIST_STD
 
 
 def mnist(root="/media/datasets/MNIST", split="val", **kwargs):
-    db = torchvision.datasets.MNIST(root, train=(split == "train"), download=True,
-                                    transform=transforms.Compose([transforms.ToTensor(),
-                                                                  transforms.Normalize(mean=MNIST_MEAN,
-                                                                                       std=MNIST_STD)]))
+    db = datasets.MNIST(root, train=(split == "train"), download=True,
+                        transform=transforms.Compose([transforms.ToTensor(),
+                                                      transforms.Normalize(mean=MNIST_MEAN,
+                                                                           std=MNIST_STD)]))
     return DataLoader(db, **kwargs)
