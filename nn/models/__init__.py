b332267769bb055f28b69d87239f698831a351d9
Maksim Penkin <penkin97@gmail.com>
2025-02-26 11:57:19 +0300

    resnet

--- nn/models/__init__.py
+++ nn/models/__init__.py
@@ -2,10 +2,9 @@
 # @author   Maksim Penkin
 # """
 
-import torch
 from nn.models.ckan import ConvKAN
 from nn.models.ukan import StackedResidualKAN
-from nn.models.resnet import resnet18, resnet50, resnet101
+from nn.models.resnet.resnet import resnet18, resnet50, resnet101
 from utils.torch_utils import torch_load
 
 from utils.serialization_utils import create_object
