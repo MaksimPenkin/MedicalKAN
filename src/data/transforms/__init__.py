0d7b7c63a76d492e44c3fbcace63210f6f567f4e
Maksim Penkin <penkin97@gmail.com>
2025-03-06 14:38:48 +0300

    transforms as PyTorch

--- src/data/transforms/__init__.py
+++ src/data/transforms/__init__.py
@@ -8,18 +8,14 @@ from src.utils.serialization_utils import create_object
 
 
 def get(identifier, **kwargs):
-    obj = create_object(identifier,
-                        module_objects={
-                            "ToTensor": ToTensor,
-                            "CompositeTransform": CompositeTransform},
-                        **kwargs)
+    obj = create_object(identifier, **kwargs)
 
     if callable(obj):
         return obj
     raise ValueError(f"Could not interpret transform instance: {obj}.")
 
 
-class CompositeTransform:
+class Compose:
 
     def __init__(self, transforms=None):
         if transforms:
@@ -31,3 +27,11 @@ class CompositeTransform:
         for t in self.transforms:
             sample = t(sample)
         return sample
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        for t in self.transforms:
+            format_string += '\n'
+            format_string += '    {0}'.format(t)
+        format_string += '\n)'
+        return format_string
