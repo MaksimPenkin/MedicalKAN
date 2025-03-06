ce1eff3659adc4abc14157c80b154dafed7897d0
Maksim Penkin <penkin97@gmail.com>
2025-03-06 16:11:34 +0300

    datetime add into the logs

--- src/trainers/base_trainer.py
+++ src/trainers/base_trainer.py
@@ -2,6 +2,7 @@
 # @author   Maksim Penkin
 # """
 
+from datetime import datetime
 from ..nn import callbacks as cbs
 from ..nn import loggers as lgs
 
@@ -12,5 +13,5 @@ class CommonLitTrainer(Trainer):
 
     def __init__(self, *args, callbacks=None, logger=None, **kwargs):
         callbacks = [cbs.get(c) for c in callbacks]
-        logger = lgs.get(logger)
+        logger = lgs.get(logger, version=datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
         super(CommonLitTrainer, self).__init__(*args, callbacks=callbacks, logger=logger, **kwargs)
