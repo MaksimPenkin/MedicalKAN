820d8fb5664abae20ca7428feca0e61edee1aaf8
Maksim Penkin <penkin97@gmail.com>
2025-05-20 16:09:51 +0300

    sync trainers.

--- src/trainers/base_trainer.py
+++ src/trainers/base_trainer.py
@@ -11,6 +11,8 @@ from lightning import Trainer
 
 class CommonLitTrainer(Trainer):
     def __init__(self, *args, callbacks=None, logger=None, **kwargs):
-        callbacks = [cbs.get(c) for c in callbacks]
-        logger = lgs.get(logger, version=datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+        if callbacks is not None:
+            callbacks = [cbs.get(c) for c in callbacks]
+        if logger is not None:
+            logger = lgs.get(logger, version=datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
         super(CommonLitTrainer, self).__init__(*args, callbacks=callbacks, logger=logger, **kwargs)
