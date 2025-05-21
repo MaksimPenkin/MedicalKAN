e013b65846480f05a8e04cf85fbfd76f87d8b8b5
Maksim Penkin <penkin97@gmail.com>
2025-05-21 11:38:42 +0300

    make nets deserializable.

--- src/models/nets/__init__.py
+++ src/models/nets/__init__.py
@@ -2,13 +2,25 @@
 # @author   Maksim Penkin
 # """
 
+from .ckan.ckan import ConvKAN
+from .resnet.resnet import resnet18, resnet50, resnet101
+from .srkan.srkan import StackedResidualKAN
+from .unet.unet import UNet
 from src.utils.torch_utils import torch_load
 
 from src.utils.serialization_utils import create_object
 
 
 def get(identifier, checkpoint=None, **kwargs):
-    obj = create_object(identifier, **kwargs)
+    obj = create_object(identifier,
+                        module_objects={
+                            "ckan": ConvKAN,
+                            "resnet18": resnet18,
+                            "resnet50": resnet50,
+                            "resnet101": resnet101,
+                            "srkan": StackedResidualKAN,
+                            "unet": UNet},
+                        **kwargs)
 
     if checkpoint:
         obj = torch_load(obj, checkpoint, strict=True)
