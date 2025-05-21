540954827bae2266cc619d50afa4e1003820d9a8
Maksim Penkin <penkin97@gmail.com>
2025-05-21 16:47:29 +0300

    feat: neuralop support hermite and hartley layers.

--- src/models/nets/neuraloperator/no.py
+++ src/models/nets/neuraloperator/no.py
@@ -3,8 +3,10 @@
 # """
 
 from torch import nn
-# from .hartley import HartleyLayer
 from .fourier import SpectralConv2d
+from .hartley import HartleyConv2d
+from .hermite import Hermite2d
+from ..layers import conv3x3
 
 
 class NeuralOperator(nn.Module):
@@ -12,28 +14,25 @@ class NeuralOperator(nn.Module):
     def __init__(self, in_ch=1, out_ch=1, hid_ch=64, version="fourier", **kwargs):
         super(NeuralOperator, self).__init__()
 
-        self.emb = nn.Linear(in_ch, hid_ch)
-
+        self.emb = conv3x3(in_ch, hid_ch)
         if version == "fourier":
             self.backbone = nn.ModuleList([SpectralConv2d(hid_ch, hid_ch, **kwargs) for _ in range(3)])
+        elif version == "hartley":
+            self.backbone = nn.ModuleList([HartleyConv2d(hid_ch, hid_ch, **kwargs) for _ in range(3)])
+        elif version == "hermite":
+            self.backbone = nn.ModuleList([Hermite2d(hid_ch, hid_ch, **kwargs) for _ in range(3)])
         else:
             raise NotImplementedError(f"Unrecognized `version` found: {version}.")
+        self.restore = conv3x3(hid_ch, out_ch)
 
-        self.restore = nn.Linear(hid_ch, out_ch)
         self.activation = nn.ReLU()
 
     def forward(self, x):
         B, C, H, W = x.shape
 
-        x = x.permute(0, 2, 3, 1)
         x = self.emb(x)
-        x = x.permute(0, 3, 1, 2)
-
         for layer in self.backbone:
-            x = self.activation(layer(x))
-
-        x = x.permute(0, 2, 3, 1)
+            x = self.activation(layer(x) + x)
         x = self.restore(x)
-        x = x.permute(0, 3, 1, 2)
 
         return x
