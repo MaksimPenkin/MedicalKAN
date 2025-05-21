c79fd65e445720bbf73ec27bdbc591f833ac9182
Maksim Penkin <penkin97@gmail.com>
2025-05-21 14:38:19 +0300

    finalize neural operator pipeline.

--- src/models/nets/neuraloperator/no.py
+++ src/models/nets/neuraloperator/no.py
@@ -7,27 +7,33 @@ from torch import nn
 from .fourier import SpectralConv2d
 
 
-class NeuralOperator(nn.Module):
+class NeuralOperator2d(nn.Module):
 
     def __init__(self, in_ch=1, out_ch=1, hid_ch=64, version="fourier", **kwargs):
-        super(NeuralOperator, self).__init__()
+        super(NeuralOperator2d, self).__init__()
 
         self.emb = nn.Linear(in_ch, hid_ch)
 
         if version == "fourier":
-            self.backbone = nn.Sequential(
-                SpectralConv2d(hid_ch, hid_ch, **kwargs),
-                SpectralConv2d(hid_ch, hid_ch, **kwargs),
-                SpectralConv2d(hid_ch, hid_ch, **kwargs)
-            )
+            self.backbone = nn.ModuleList([SpectralConv2d(hid_ch, hid_ch, **kwargs) for _ in range(3)])
         else:
             raise NotImplementedError(f"Unrecognized `version` found: {version}.")
 
         self.restore = nn.Linear(hid_ch, out_ch)
+        self.activation = nn.ReLU()
 
     def forward(self, x):
+        B, C, H, W = x.shape
+
+        x = x.permute(0, 2, 3, 1)
         x = self.emb(x)
-        x = self.backbone(x)
+        x = x.permute(0, 3, 1, 2)
+
+        for layer in self.backbone:
+            x = self.activation(layer(x))
+
+        x = x.permute(0, 2, 3, 1)
         x = self.restore(x)
+        x = x.permute(0, 3, 1, 2)
 
         return x
