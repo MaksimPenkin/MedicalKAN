e031167a18fdd587e9f3dc87d723d02d03bacb77
Maksim Penkin <penkin97@gmail.com>
2025-05-26 14:06:51 +0300

    switch to v1.2.

--- src/models/nets/srkan/attention.py
+++ src/models/nets/srkan/attention.py
@@ -27,13 +27,13 @@ class AttentionKANLinear(nn.Module):
         # self.proj = nn.Linear(input_dim * len(self.subspaces), output_dim)
 
         # v1.1
-        self.mha = nn.MultiheadAttention(input_dim, num_heads=1, batch_first=True)
-        self.coeffs = nn.Parameter(torch.empty(input_dim, output_dim, (degree + 1) * len(self.subspaces)))
-        nn.init.normal_(self.coeffs, mean=0.0, std=1 / (input_dim * (degree + 1) * len(self.subspaces)))
+        # self.mha = nn.MultiheadAttention(input_dim, num_heads=1, batch_first=True)
+        # self.coeffs = nn.Parameter(torch.empty(input_dim, output_dim, (degree + 1) * len(self.subspaces)))
+        # nn.init.normal_(self.coeffs, mean=0.0, std=1 / (input_dim * (degree + 1) * len(self.subspaces)))
 
         # v1.2
-        # self.mha = nn.MultiheadAttention(input_dim, num_heads=1, batch_first=True)
-        # self.proj = nn.Linear(input_dim, output_dim)
+        self.mha = nn.MultiheadAttention(input_dim, num_heads=1, batch_first=True)
+        self.proj = nn.Linear(input_dim, output_dim)
 
     def forward(self, x):
         x = torch.reshape(x, (-1, self.inputdim))
@@ -45,15 +45,15 @@ class AttentionKANLinear(nn.Module):
         # return x.view(-1, self.outdim)
 
         # v1.1
-        x = torch.cat([layer(x) for layer in self.subspaces], dim=-1).permute(0, 2, 1)
-        x, _ = self.mha(x, x, x)
-        x = x.permute(0, 2, 1)
-        x = torch.einsum('bid,iod->bo', x, self.coeffs)
-        return x.view(-1, self.outdim)
+        # x = torch.cat([layer(x) for layer in self.subspaces], dim=-1).permute(0, 2, 1)
+        # x, _ = self.mha(x, x, x)
+        # x = x.permute(0, 2, 1)
+        # x = torch.einsum('bid,iod->bo', x, self.coeffs)
+        # return x.view(-1, self.outdim)
 
         # v1.2
-        # q_x = x.unsqueeze(1)
-        # k_x = torch.cat([layer(x) for layer in self.subspaces], dim=-1).permute(0, 2, 1)
-        # x, m = self.mha(q_x, k_x, k_x)
-        # x = self.proj(x.squeeze(1))
-        # return x.view(-1, self.outdim)
+        q_x = x.unsqueeze(1)
+        k_x = torch.cat([layer(x) for layer in self.subspaces], dim=-1).permute(0, 2, 1)
+        x, m = self.mha(q_x, k_x, k_x)
+        x = self.proj(x.squeeze(1))
+        return x.view(-1, self.outdim)
