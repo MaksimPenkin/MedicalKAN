4d6f1a9a84d9dd32436e3a547a4d3ebe644c39bd
Maksim Penkin <penkin97@gmail.com>
2025-06-09 16:26:09 +0300

    no arch upd

--- src/models/nets/neuraloperator/no.py
+++ src/models/nets/neuraloperator/no.py
@@ -3,20 +3,23 @@
 # """
 
 from torch import nn
+import torch.nn.functional as F
+
 from .fourier import FourierSpectralConv2d
 from .hartley import HartleySpectralConv2d
 from .mlp import MLP
 from .kan import KAN
 
-from ..layers import conv3x3
+from ..layers import conv1x1, conv3x3, conv5x5
 
 
 class NeuralOperator(nn.Module):
 
-    def __init__(self, in_ch=1, out_ch=1, hid_ch=64, version="fourier", **kwargs):
+    def __init__(self, in_ch=1, out_ch=1, hid_ch=32, version="fourier", **kwargs):
         super(NeuralOperator, self).__init__()
 
-        self.emb = conv3x3(in_ch, hid_ch)
+        self.embedding = conv5x5(in_ch, hid_ch // 2)
+        self.lifting = conv3x3(hid_ch // 2, hid_ch)
         if version == "fourier2d":
             self.backbone = nn.ModuleList([FourierSpectralConv2d(hid_ch, hid_ch, activation="relu", **kwargs) for _ in range(3)])
         elif version == "hartley2d":
@@ -27,12 +30,14 @@ class NeuralOperator(nn.Module):
             self.backbone = nn.ModuleList([KAN(hid_ch, hid_ch, **kwargs) for _ in range(3)])
         else:
             raise NotImplementedError(f"Unrecognized `version` found: {version}.")
-        self.restore = conv3x3(hid_ch, out_ch)
+        self.projection = conv1x1(hid_ch, hid_ch // 2)
+        self.restoration = conv1x1(hid_ch // 2, out_ch)
 
     def forward(self, x):
-        x = self.emb(x)
+        x = F.relu(self.embedding(x))
+        x = self.lifting(x)
         for layer in self.backbone:
             x = x + layer(x)
-        x = self.restore(x)
-
+        x = self.projection(x)
+        x = self.restoration(F.relu(x))
         return x
