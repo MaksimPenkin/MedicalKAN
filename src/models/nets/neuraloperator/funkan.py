db4538b9f43e6d5e95d8743272c2bb3c573b6c6d
Maksim Penkin <penkin97@gmail.com>
2025-06-10 14:16:43 +0300

    FUNKAN implemented.

--- src/models/nets/neuraloperator/funkan.py
+++ src/models/nets/neuraloperator/funkan.py
@@ -2,20 +2,43 @@
 # @author   Maksim Penkin
 # """
 
+import math
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-from ..srkan.linear import KANLinear
-from ..srkan.chebyshev import ChebyKANLinear
-from ..srkan.hermite import HermiteFuncKANLinear
+
+class HermiteFuncs(nn.Module):
+    # https://github.com/Rob217/hermite-functions/blob/master/hermite_functions/hermite_functions.py
+    def __init__(self, r):
+        super(HermiteFuncs, self).__init__()
+        self.r = r
+
+    def forward(self, x):
+        B, HW, C = x.shape
+
+        x_d = torch.tanh(torch.linspace(-1, 1, C)) * (math.sqrt(2 * self.r + 1) + 1)
+
+        psi = torch.ones(self.r + 1, C, device=x.device)
+        psi[0, :] = math.pi ** (-1 / 4) * torch.exp(-(x_d ** 2) / 2)
+        if self.r > 0:
+            psi[1, :] = math.sqrt(2) * math.pi ** (-1 / 4) * x_d * torch.exp(-(x_d ** 2) / 2)
+        for k in range(2, self.r + 1):
+            psi[k, :] = math.sqrt(2 / k) * x_d * psi[k - 1, :].clone() - math.sqrt((k - 1) / k) * psi[k - 2, :].clone()
+
+        return psi.unsqueeze(0).repeat(B, 1, 1)
 
 
 class FUNKAN(nn.Module):
 
-    def __init__(self, in_channels, out_channels, **kwargs):
+    def __init__(self, in_channels, out_channels, degree=5, **kwargs):
         super(FUNKAN, self).__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
+
+        self.basis = HermiteFuncs(degree)
+        self.fc = nn.Conv1d(in_channels, out_channels, 1)
 
         self.norm = nn.LayerNorm(in_channels)
         self.apply(self._init_weights)
@@ -27,5 +50,14 @@ class FUNKAN(nn.Module):
 
     def forward(self, x):
         B, C, H, W = x.shape
+        x = x.flatten(start_dim=2).transpose(1, 2)
+        x = self.norm(x)
 
-        return x
+        q_x = x
+        x = self.basis(x)
+        scores = torch.bmm(q_x, x.transpose(1,2)) / (self.in_channels ** 0.5)
+        x = torch.bmm(F.softmax(scores, dim=2), x)
+
+        x = self.fc(x.transpose(1, 2)).view(B, self.out_channels, H*W).contiguous()
+
+        return x.view(B, self.out_channels, H, W)
