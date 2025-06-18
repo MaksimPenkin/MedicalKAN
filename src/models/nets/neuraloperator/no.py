cf10162d90eba3f433f5041e25ca31dcaba2fad0
Maksim Penkin <penkin97@gmail.com>
2025-06-18 08:40:31 +0300

    naming fix: version -> backbone.

--- src/models/nets/neuraloperator/no.py
+++ src/models/nets/neuraloperator/no.py
@@ -14,21 +14,21 @@ from ..layers import conv1x1, conv3x3, conv5x5, activate
 
 class NeuralOperator(nn.Module):
 
-    def __init__(self, in_ch=1, out_ch=1, hid_ch=32, version="fourier", activation="relu", **kwargs):
+    def __init__(self, in_ch=1, out_ch=1, hid_ch=32, backbone="fourier", activation="relu", **kwargs):
         super(NeuralOperator, self).__init__()
 
         self.embedding = conv5x5(in_ch, hid_ch // 2)
         self.lifting = conv3x3(hid_ch // 2, hid_ch)
-        if version == "fourier2d":
+        if backbone == "fourier2d":
             self.backbone = nn.ModuleList([FourierSpectralConv2d(hid_ch, hid_ch, activation=activation, **kwargs) for _ in range(3)])
-        elif version == "hartley2d":
+        elif backbone == "hartley2d":
             self.backbone = nn.ModuleList([HartleySpectralConv2d(hid_ch, hid_ch, activation=activation, **kwargs) for _ in range(3)])
-        elif version == "kan":
+        elif backbone == "kan":
             self.backbone = nn.ModuleList([KAN(hid_ch, hid_ch, **kwargs) for _ in range(3)])
-        elif version == "funkan":
+        elif backbone == "funkan":
             self.backbone = nn.ModuleList([FUNKAN(hid_ch, hid_ch, activation=activation, **kwargs) for _ in range(3)])
         else:
-            raise NotImplementedError(f"Unrecognized `version` found: {version}.")
+            raise NotImplementedError(f"Unrecognized `backbone` found: {backbone}.")
         self.projection = conv1x1(hid_ch, hid_ch // 2)
         self.restoration = conv1x1(hid_ch // 2, out_ch)
 
