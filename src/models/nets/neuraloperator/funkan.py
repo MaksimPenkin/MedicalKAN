698371e605c6a0e6ebb7e5cb91e14e98156d8ce6
Maksim Penkin <penkin97@gmail.com>
2025-06-18 08:35:17 +0300

    bugfix: psi should depend on chi_i.

--- src/models/nets/neuraloperator/funkan.py
+++ src/models/nets/neuraloperator/funkan.py
@@ -5,36 +5,30 @@
 import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from ..layers import activate
 
 
-def hermite_funcs(N, r=6):
-    x = torch.tanh(torch.linspace(-1, 1, N)) * (math.sqrt(2 * r + 1) + 1)
+class HermiteFuncs(nn.Module):
 
-    psi = torch.ones(r, N, device=x.device)
-    psi[0, :] = math.pi ** (-1 / 4) * torch.exp(-(x ** 2) / 2)
-    if r > 0:
-        psi[1, :] = math.sqrt(2) * math.pi ** (-1 / 4) * x * torch.exp(-(x ** 2) / 2)
-    for k in range(2, r):
-        psi[k, :] = math.sqrt(2 / k) * x * psi[k - 1, :].clone() - math.sqrt((k - 1) / k) * psi[k - 2, :].clone()
-
-    return psi
-
-
-class BasisFuncs(nn.Module):
-
-    def __init__(self, r=6, basis="hermite"):
-        super(BasisFuncs, self).__init__()
+    def __init__(self, r=6):
+        super(HermiteFuncs, self).__init__()
 
         self.r = r
-        self.basis = basis
 
     def forward(self, x):
-        if self.basis == "hermite":
-            return hermite_funcs(x.shape[-1], self.r)
-        else:
-            raise NotImplementedError(f"Unrecognized `basis` found: {self.basis}.")
+        B, C, HW = x.shape
+
+        x = torch.tanh(x) * math.sqrt(2 * self.r + 1)
+        psi = torch.ones(B, self.r, C, HW, device=x.device)
+        psi[:, 0, :, :] = math.pi ** (-1 / 4) * torch.exp(-(x ** 2) / 2)
+        if self.r > 0:
+            psi[:, 1, :, :] = math.sqrt(2) * math.pi ** (-1 / 4) * x * torch.exp(-(x ** 2) / 2)
+        for k in range(2, self.r):
+            psi[:, k, :, :] = math.sqrt(2 / k) * x * psi[:, k - 1, :, :].clone() - math.sqrt((k - 1) / k) * psi[:, k - 2, :, :].clone()
+
+        return psi
 
 
 class FUNKAN(nn.Module):
@@ -45,27 +39,21 @@ class FUNKAN(nn.Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
 
-        self.psi = BasisFuncs(**kwargs)
-        self.fc = nn.Conv1d(in_channels, out_channels, 1, bias=False)
+        self.psi = HermiteFuncs(**kwargs)
+        self.fc = nn.Conv1d(in_channels, out_channels, 1)
         self.activation = activate(activation)
 
-        self.norm = nn.LayerNorm(in_channels)
-        self.apply(self._init_weights)
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
+        self.norm = nn.BatchNorm1d(in_channels)
 
     def forward(self, x):
         B, C, H, W = x.shape
-        x = x.flatten(start_dim=2)
 
-        x = self.norm(x)  # (B, n, N): Get n feature functions, discretized by N nodes.
-        psi = self.psi(x)  # (B, r, N): Get r basis functions, discretized by N nodes.
-        cost = torch.bmm(x, psi.transpose(1, 2))  # (B, n, r): Calculate cost matrix of feature functions projections on basis functions.
-        x = torch.bmm(cost, psi)  # (B, n, N): Construct Kolmogorov-Arnold functions as decomposition over basis functions (with attention).
+        x = x.flatten(start_dim=2)  # (B, n, N)
+        x = self.norm(x)
 
-        x = self.fc(x).view(B, self.out_channels , H * W).contiguous()  # Convolve over all feature functions like in K-A theorem.
+        psi = self.psi(x)  # (B, r, n, N)
+        cost = torch.einsum("bkid,bid->bki", psi, x)  # (B, r, n)
+        x = torch.einsum("bki,bkid->bid", F.softmax(cost, dim=1), psi)
+        x = self.fc(x).view(B, self.out_channels, H, W).contiguous()  # Convolve over all feature functions like in K-A theorem.
 
         return self.activation(x)
