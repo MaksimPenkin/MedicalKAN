fc7c3b8ac0e4aca1173b64ef8eb43b03c1a0faa8
Maksim Penkin <penkin97@gmail.com>
2025-07-01 17:21:31 +0300

    minor.

--- src/models/nets/__init__.py
+++ src/models/nets/__init__.py
@@ -3,12 +3,12 @@
 # """
 
 from .ckan.ckan import ConvKAN
+from .neuraloperator.no import NeuralOperator
 from .resnet.resnet import resnet18, resnet50, resnet101
 from .srkan.srkan import StackedResidualKAN
 from .unet.unet import UNet
-from .neuraloperator.no import NeuralOperator
-from src.utils.torch_utils import torch_load
 
+from src.utils.torch_utils import torch_load
 from src.utils.serialization_utils import create_object
 
 
@@ -16,12 +16,12 @@ def get(identifier, checkpoint=None, **kwargs):
     obj = create_object(identifier,
                         module_objects={
                             "ConvKAN": ConvKAN,
+                            "NeuralOperator": NeuralOperator,
                             "resnet18": resnet18,
                             "resnet50": resnet50,
                             "resnet101": resnet101,
                             "StackedResidualKAN": StackedResidualKAN,
-                            "UNet": UNet,
-                            "NeuralOperator": NeuralOperator},
+                            "UNet": UNet},
                         **kwargs)
 
     if checkpoint:
