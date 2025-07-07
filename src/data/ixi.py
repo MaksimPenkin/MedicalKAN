e1cab82a11da2754b11ac4d0a04d74e982e8caf9
Maksim Penkin <penkin97@gmail.com>
2025-07-07 14:07:22 +0300

    minor: ixi.

---
+++ src/data/ixi.py
@@ -0,0 +1,16 @@
+# """
+# @author   Maksim Penkin
+# """
+
+from . import datasets
+import albumentations as A
+from torch.utils.data import DataLoader
+
+
+def ixi(dataset, **kwargs):
+    transform = A.Compose([
+        A.ToTensorV2()
+    ])
+
+    db = datasets.get(dataset, transform=transform)
+    return DataLoader(db, **kwargs)
