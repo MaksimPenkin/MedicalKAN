d16de9b3fded99ca88d9fe1316df2c53ac61188d
Maksim Penkin <penkin97@gmail.com>
2025-07-07 13:57:16 +0300

    feat: integrate busi dataset and minor.

---
+++ src/data/busi.py
@@ -0,0 +1,26 @@
+# """
+# @author   Maksim Penkin
+# """
+
+from . import datasets
+import albumentations as A
+from torch.utils.data import DataLoader
+
+
+def busi(dataset, split="val", **kwargs):
+    if split == "train":
+        transform = A.Compose([
+            A.Resize(256, 256),
+            A.VerticalFlip(p=0.5),
+            A.HorizontalFlip(p=0.5),
+            A.RandomRotate90(p=0.5),
+            A.Transpose(p=0.5),
+            A.Rotate(p=0.5),
+        ])
+    else:
+        transform = A.Compose([
+            A.Resize(256, 256),
+        ])
+
+    db = datasets.get(dataset, transform=transform)
+    return DataLoader(db, **kwargs)
--- src/data/datasets/__init__.py
+++ src/data/datasets/__init__.py
@@ -4,7 +4,6 @@
 
 from torch.utils.data import Dataset
 from .file_dataset import FileDataset
-from .random_dataset import RandomUniformDataset
 from .sampler_dataset import SamplerDataset
 
 from src.utils.serialization_utils import create_object
@@ -14,7 +13,6 @@ def get(identifier, **kwargs):
     obj = create_object(identifier,
                         module_objects={
                             "FileDataset": FileDataset,
-                            "RandomUniformDataset": RandomUniformDataset,
                             "SamplerDataset": SamplerDataset},
                         **kwargs)
 
